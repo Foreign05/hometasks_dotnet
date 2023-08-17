f8d0009d4e09c80ba4c006d17362a6d57f80af90
Legion <nonamef4ke@gmail.com>
2023-08-18 01:16:37 +0500

    2ndPart

---
+++ Examples/Hometasks/Hometask005_secondChar/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+while (true)
+{
+    Console.WriteLine("Type number from 100 to 1000: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    if (input > 100 && input < 1000)
+    {
+        int dev = input / 10;
+        int dev1 = dev % 10;
+        Console.WriteLine($"Second number is {dev1}!");
+        break;
+    }
+}
+
+
+
---
+++ Examples/Hometasks/Hometask007_weekend/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int holiday1 = 6;
+int holiday2 = 0;
+Console.WriteLine("What is the day today?");
+int input = Convert.ToInt32(Console.ReadLine());
+int week = input % 7;
+
+if (week == holiday1 || week == holiday2)
+{
+    Console.WriteLine("Today is holiday!");
+}
+else
+{
+    Console.WriteLine("Today is not holiday..");
+}
