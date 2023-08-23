68eb899dd24c0e43c72bfa422d3b9321e5f013c6
Legion <nonamef4ke@gmail.com>
2023-08-23 22:03:00 +0500

    confused

---
+++ Examples/Hometasks/Hometask008_polindrom/Program.cs
@@ -0,0 +1,52 @@
+﻿
+// Напишите программу которая принимает на вход пятизначное число, и проверяет, является ли число палиндромом
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+//                                                 НЕОКОНЧЕНО
+
+
+
+string[] arr = new string[5];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Console.ReadLine();
+
+    int len = arr[i].Length;
+
+    foreach (var value in arr)
+    {
+        int number;
+        bool success = int.TryParse(value, out number);
+
+        if (success)
+        {
+            
+            while (len == 5)
+            {
+                if (true)
+                {
+                    Array.Reverse(arr);
+                    foreach (var item in arr)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your numer have to consist 5 chars");
+                }
+                break;
+            }
+
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Type just numbers");
+        }
+    }
+    
+}
